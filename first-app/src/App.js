ce5ddda1e8e64f4fab0697314e2181aff26bd236
ShanmukhSaiMadhu <madhushanmukhsai@gmail.com>
2022-09-14 11:54:51 +0530

    dark mode added

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,28 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  function toggleDarkMode() {
+    return setDarkMode(prevMode => !prevMode)
+  }
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Navbar />
-      <Main />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
--- first-app/src/components/Navbar.js
+++ first-app/src/components/Navbar.js
@@ -1,12 +1,29 @@
 import React from "react"
 import logo from '../logo.svg'
 
-function Navbar () {
+function Navbar (props) {
     return (
-        <nav className="nav-bar">
-            <img src={logo} className="nav-logo" />
-            <h3 className="nav-logo-text">ReactFacts</h3>
-            <h4 className="nav-title">React Course - Project</h4>
+        <nav 
+            className={props.darkMode ? "dark": ""}
+        >
+            <img 
+                className="nav-logo_icon"
+                src={logo}
+            />
+            <h3 className="nav-logo_text">ReactFacts</h3>
+            
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler-light">Light</p>
+                <div 
+                    className="toggler-slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler-slider-circle"></div>
+                </div>
+                <p className="toggler-dark">Dark</p>
+            </div>
         </nav>
     )
 }
